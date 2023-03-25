6c864d59c802939d1162f419789db3db450b5fb2
w3App <kshatriyakabawat@gmail.com>
2023-03-25 22:26:23 +0530

    send mail
    send mail after user successfully registered

--- controller/signup/index.js
+++ controller/signup/index.js
@@ -1,13 +1,49 @@
+const nodemailer = require('nodemailer')
 const { userModal } = require('../')
 const jwt = require('jsonwebtoken')
 const privateKey = process.env.PRIVATE_KEY_JWT
+
+const sendEmail = (transport, mailoption, res) => {
+    transport.sendMail(mailoption, (error, result) => {
+        if (error) {
+            res.status(500).json({
+                massage: error,
+                status: false,
+            })
+        }
+    })
+}
+
 module.exports.registration = async (req, res) => {
-    const { email, username, password } = req.body
-    console.log(privateKey)
     try {
+        const { email, username, password } = req.body
+        const transport = nodemailer.createTransport({
+            service: "gmail",
+            secure: true,
+            auth: {
+                user: 'testbymukesh@gmail.com',
+                pass: 'umywypawbwiurzkb'
+            }
+        })
+        const mailoption = {
+            from: 'testbymukesh@gmail.com',
+            to: email,
+            subject: 'confirmation email',
+            html: `<div style="font-family: sans-serif; text-align: center;">
+            <h1>Welcome to QueryBoat!</h1>
+            <p>Hello ${username},</p>
+            <p>We're thrilled that you've joined QueryBoat. You can now connect with people around the world and explore new possibilities.</p>
+            <p>Your account has been registered with the following details:</p>
+            <ul style="list-style-type: none; padding: 0;">
+              <li><strong>Email:</strong> ${email}</li>
+              <li><strong>Username:</strong> ${username}</li>
+            </ul>
+            <p>Thank you for choosing QueryBoat. We hope you enjoy using our service!</p>
+          </div>`
+        }
         const update = await userModal.updateOne({ email: email }, {
             username,
-            opt: null,
+            otp: null,
             status: true,
             password,
         })
@@ -15,6 +51,7 @@ module.exports.registration = async (req, res) => {
             throw new Error('Something went wrong')
         }
         const token = jwt.sign({ email, username }, privateKey)
+        sendEmail(transport, mailoption, res)
         res.status(200).json({
             status: true,
             massage: 'registration successful',
