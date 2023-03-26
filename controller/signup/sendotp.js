85a19ce30d1b42e1de0048c21af140b90a5ae375
w3App <kshatriyakabawat@gmail.com>
2023-03-26 23:32:22 +0530

    OTP

--- controller/signup/sendotp.js
+++ controller/signup/sendotp.js
@@ -29,7 +29,11 @@ module.exports.send_otp = async (req, res) => {
             pass: 'umywypawbwiurzkb'
         }
     })
-    const otp = Math.floor(Math.random() * 1000000)
+    var digits = '0123456789';
+    let otp = '';
+    for (let i = 0; i < 6; i++) {
+        otp += digits[Math.floor(Math.random() * 10)];
+    } 
     const mailoption = {
         from: 'testbymukesh@gmail.com',
         to: email,
