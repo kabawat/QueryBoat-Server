f54ae031429764f3b6f4e4a1f61e91c3917a034f
w3App <kshatriyakabawat@gmail.com>
2023-03-27 19:46:01 +0530

    env variable

--- controller/signup/sendotp.js
+++ controller/signup/sendotp.js
@@ -1,10 +1,9 @@
 const { userModal } = require('..')
 const nodemailer = require('nodemailer')
-
+const { EMAIL_PASSWORD, EMAIL_USER } = process.env
 const sendEmail = (transport, mailoption, res) => {
     transport.sendMail(mailoption, (error, result) => {
         if (error) {
-            console.log(error)
             res.status(500).json({
                 massage: error,
                 status: false,
@@ -25,17 +24,17 @@ module.exports.send_otp = async (req, res) => {
         service: "gmail",
         secure: true,
         auth: {
-            user: 'testbymukesh@gmail.com',
-            pass: 'umywypawbwiurzkb'
+            user: EMAIL_USER,
+            pass: EMAIL_PASSWORD
         }
     })
     var digits = '0123456789';
     let otp = '';
     for (let i = 0; i < 6; i++) {
         otp += digits[Math.floor(Math.random() * 10)];
-    } 
+    }
     const mailoption = {
-        from: 'testbymukesh@gmail.com',
+        from: EMAIL_USER,
         to: email,
         subject: 'email verification',
         html: `<p>your verification otp is <b>${otp}</b></p>`
