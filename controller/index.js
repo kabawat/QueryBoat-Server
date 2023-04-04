ef536e934ae6d399e6f56252c24cc1a930aac8c8
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-04-04 18:49:27 +0530

    socket connection

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', true);
 const URL = `mongodb+srv://QueryBoat:ty9we3ys86@queryboat.x19vz8s.mongodb.net/QueryBoat?retryWrites=true&w=majority`
 mongoose.connect(URL, {
     useNewUrlParser: true,
@@ -24,8 +25,38 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    chatid: {
+        type: String,
+    },
     token: String,
     profile_image: String
 }, { timestamps: true })
+
+
+// user Chat table 
+const chatTable = new mongoose.Schema({
+    chatID: {
+        type: String,
+        require
+    },
+    sender: {
+        type: String,
+        require
+    },
+    receiver: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const chatSocket = new mongoose.Schema({}, { strict: false })
+
+
+const chatModal = new mongoose.model('chatList', chatTable)
+const socketModal = new mongoose.model('socketIds', chatSocket)
 const userModal = new mongoose.model('userdata', userSchema)
-module.exports = { userModal }
+
+module.exports = { userModal, chatModal, socketModal }
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 const app = express()
+const { socketModal } = require('./controller')
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:3000', 'https://socket-client.onrender.com']
+    }
+})
+
 const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
-
 const path = require('path')
 const dot = require('dotenv').config()
 const router = require('./router')
@@ -17,6 +26,57 @@ app.use(fileUpload())
 app.use('/user', express.static(path.join(__dirname, 'public/user')))
 app.use(router)
 
-app.listen(port, () => {
+
+// socket data 
+const startSocketServer = () => {
+    io.on('connection', (socket) => {
+        socket.on('disconnect', () => {
+            console.log('A user disconnected:', socket.id);
+        });
+
+        // when new user join Query Boat chat 
+        socket.on('New User Join', data => {
+            socketTablehandler(data)
+        })
+
+        // when user refresh the page 
+        socket.on('refresh', data => {
+            socketTableUpdate(data)
+        })
+
+    });
+};
+
+// server listen 
+server.listen(port, () => {
+    startSocketServer()
     console.log(`http://localhost:${port}`)
 })
+
+
+// insert new data 
+const socketTablehandler = async (data) => {
+    try {
+        const modal = new socketModal({
+            username: data.username,
+            [data.username]: data.id
+        })
+        await modal.save()
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+// update 
+const socketTableUpdate = async (data) => {
+    try {
+        const update = await socketModal.updateOne({ username: data.username }, {
+            [data.username]: data.id
+        })
+        if (update.modifiedCount !== 1) {
+            throw new Error('Something went wrong')
+        }
+    } catch (error) {
+
+    }
+}
